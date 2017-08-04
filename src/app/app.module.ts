2c30e8f8a0ea92e5f67f217c189632594a1297a7
Misha3739 <mihail.udot@yandex.ru>
2017-08-04 21:41:05 +0300

    Routing moved to separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing-module';
 
-const appRoutes: Routes = [
-  { path: '' , component: HomeComponent},
-  { path: 'users' , component: UsersComponent, children: [
-    { path: ':id/:name' , component: UserComponent},
-  ]},
-
-  { path: 'servers' , component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-    ]
-  },
-  {path: '**', component: PageNotFoundComponent}
-
-];
 
 @NgModule({
   declarations: [
@@ -44,7 +31,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
